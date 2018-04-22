5cac5e68d59ab1f4075fa870489176f8b33796ae
KNIGHTMASTER <fauzi.knightmaster.achmad@gmail.com>
2018-04-22 20:17:24 +0700

    Added Testing and Logging Interceptor

--- src/main/java/com/zisal/springbootrestcxf/CXFConfiguration.java
+++ src/main/java/com/zisal/springbootrestcxf/CXFConfiguration.java
@@ -2,19 +2,21 @@ package com.zisal.springbootrestcxf;
 
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
-import org.apache.cxf.jaxrs.lifecycle.ResourceProvider;
-import org.apache.cxf.jaxrs.spring.SpringResourceFactory;
 import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 import javax.ws.rs.Path;
-import java.util.Arrays;
-import java.util.LinkedList;
+import javax.ws.rs.ext.Provider;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,16 +30,37 @@ public class CXFConfiguration {
 
     @Autowired private ApplicationContext ctx;
 
-    /*@Bean
-    public ServletRegistrationBean cxfServletRegistrationBean(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new CXFServlet(), "/services*//*");
-        registrationBean.setAsyncSupported(true);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.setName("CXFServlet");
-        return registrationBean;
-    }*/
+    private static final Logger LOGGER = LoggerFactory.getLogger(CXFConfiguration.class);
+
+    @Value("${cxf.log.request}")
+    private boolean logRequests;
 
     @Bean
+    public Server jaxRsServer() {
+        // Find all beans annotated with @Path
+        List<Object> serviceBeans = new ArrayList<>(ctx.getBeansWithAnnotation(Path.class).values());
+        LOGGER.info("Registering service beans: {}" , serviceBeans);
+
+        // Find all beans annotated with @Providers
+        List<Object> providers = new ArrayList<>(ctx.getBeansWithAnnotation(Provider.class).values());
+        providers.add(new JacksonJsonProvider());
+        LOGGER.info("Registering providers: {}",  providers);
+
+        JAXRSServerFactoryBean factory = new JAXRSServerFactoryBean();
+        factory.setBus(ctx.getBean(SpringBus.class));
+        factory.setAddress("/v1");
+        factory.setServiceBeans(serviceBeans);
+        factory.setProviders(providers);
+        Server server = factory.create();
+
+        if (logRequests) {
+            server.getEndpoint().getInInterceptors().add(new LoggingInInterceptor());
+        }
+        return server;
+    }
+
+    /*REGISTRATION 1*/
+    /*@Bean
     public Server rxServer() {
         List<ResourceProvider> resourceProviders = new LinkedList<>();
         for (String beanName : ctx.getBeanDefinitionNames()) {
@@ -57,5 +80,5 @@ public class CXFConfiguration {
         } else {
             return null;
         }
-    }
+    }*/
 }
--- src/test/java/com/zisal/springbootrestcxf/SpringBootRestCxfApplicationTests.java
+++ src/test/java/com/zisal/springbootrestcxf/SpringBootRestCxfApplicationTests.java
@@ -1,16 +1,35 @@
 package com.zisal.springbootrestcxf;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import io.restassured.RestAssured;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootRestCxfApplicationTests {
 
+	private static final int PORT = 8084;
+	private static WireMockServer wireMockServer = new WireMockServer(PORT);
+
+	private static final String APPLICATION_JSON = "application/json";
+
 	@Test
 	public void contextLoads() {
+		System.out.println("Setting up!");
+		wireMockServer.start();
+		WireMock.configureFor("localhost", PORT);
+		RestAssured.port = PORT;
+		WireMock.stubFor(get(urlEqualTo("/employee/findall")).willReturn(
+				aResponse().withStatus(200)
+						.withHeader("Content-Type", APPLICATION_JSON)
+						.withHeader("Accept", APPLICATION_JSON)
+						.withBody("")));
 	}
 
 }
